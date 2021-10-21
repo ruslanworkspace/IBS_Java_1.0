6821136c465367d4a327d109d2883ebdf9bb566c
ruslanzhelezo <ruslanzhelezo@gmail.com>
2021-10-21 23:20:48 +0500

    Byte.MAX_VALUE

--- src/Task_2.java
+++ src/Task_2.java
@@ -1,6 +1,7 @@
 public class Task_2 {
     public static void main(String[] args) {
         double one = 3.1415926535;
-        byte two = 127;
+        //byte two = 127;
+        byte three = Byte.MAX_VALUE;
     }
 }
