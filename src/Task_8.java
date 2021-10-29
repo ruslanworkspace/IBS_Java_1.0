4e6c32b4eb598891903bd7eb0c44c93d629d68f6
ruslanzhelezo <ruslanzhelezo@gmail.com>
2021-10-29 21:16:55 +0500

    Задача 8 - Циклы, сумма нечетных от 1 до n

---
+++ src/Task_8.java
@@ -0,0 +1,21 @@
+// Напишите программу, где пользователь вводит любое целое положительное число n. А программа суммирует все нечетные числа от 1 до введенного пользователем числа n.
+
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        int input, result=0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите целое положительное число: ");
+        input = in.nextInt();
+        if (input < 0) {
+            System.out.print("Число отрицательное! Выход ");
+            System.exit(0);
+        }
+        for (int i = 1; i < input; i+=2){
+            result += i;
+        }
+        System.out.printf("Сумма нечетных чисел от 1 до %d - %d", input, result);
+    }
+}
