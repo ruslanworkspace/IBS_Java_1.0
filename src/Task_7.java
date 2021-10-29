504bb9f977d0768f7e4335cd23408bf21ba0dd8f
ruslanzhelezo <ruslanzhelezo@gmail.com>
2021-10-29 22:02:18 +0500

    Задача 7 - Циклы. Наличие констант в массиве.

---
+++ src/Task_7.java
@@ -0,0 +1,31 @@
+/*
+Напишите программу:
+    1. Пользователь вводит размер массива и данные с клавиатуры в массив
+    2. Сравнить элементы массива с заранее заданными константами x, y, z.
+    3. Если массив содержит хотя бы одну из констант, вывести текст "Данное значение имеется в константах".
+*/
+
+import java.util.Scanner;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        final int X = 10;
+        final int Y = 20;
+        final int Z = 30;
+        int arrLen;
+        int[] arr;
+        boolean flag_arrContainsConst = false;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        arrLen = in.nextInt();
+        System.out.printf("Введите %d элементов массива:\n", arrLen);
+        arr = new int[arrLen];
+        for (int i = 0; i < arrLen; i++){
+            arr[i] = in.nextInt();
+            if (arr[i] == X || arr[i] == Y || arr[i] == Z){
+                flag_arrContainsConst = true;
+            }
+        }
+        if (flag_arrContainsConst) System.out.println("Данное значение имеется в константах");
+    }
+}
