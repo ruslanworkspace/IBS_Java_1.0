097cc507109183f9f44e23abb32291d01a8d0038
ruslanzhelezo <ruslanzhelezo@gmail.com>
2021-11-01 22:01:00 +0500

    Задача 14 - Работа с данными. Переделано на "учебный" вариант.

--- src/Task_14.java
+++ src/Task_14.java
@@ -2,26 +2,22 @@
 //    1. Выведите максимальный и минимальный элемент массива.
 //    2. Из максимального и минимального значения выведите наибольшее по модулю.
 
-
-import java.util.Arrays;
 import java.util.Random;
 
 public class Task_14 {
     public static void main(String[] args) {
         Random random = new Random();
-
+        int max, min;
         int[] array = new int[15];
-        for (byte i = 0; i < array.length; i++){
+        array[0] = min = max = random.nextInt(41) - 20;
+        for (byte i = 1; i < array.length; i++){
             array[i] = (random.nextInt(41) - 20);
-            // System.out.println(array[i]);
+            if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i];
         }
-
-        int max = Arrays.stream(array).max().getAsInt();
-        int min = Arrays.stream(array).min().getAsInt();
 
         System.out.printf("Максимальный элемент массива: %d\n", max);
         System.out.printf("Минимальный элемент массива: %d\n", min);
         System.out.printf("Наибольший по модулю: %d\n", Math.max(Math.abs(min),Math.abs(max)));
-
     }
 }
